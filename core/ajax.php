d6c450e6477a683641b28b2e4d157e03e8fbd50c
test1 <test1@mail.me>
2018-06-20 20:38:39 +0200

    vue + ajax + php api

--- core/ajax.php
+++ core/ajax.php
@@ -1,18 +1,7 @@
 <?php
 
-$tabResponse = [];
-
-$classForm = Core::Request()->getInput("classForm");
-$methodForm = Core::Request()->getInput("methodForm");
-
-if ($classForm != "")
+if (isset($tabResponse))
 {
-    // DANGER: SHOULD FILTER ON PUBLIC CLASSES
-    $tabResponse["$classForm@$methodForm"] = Core::getOne("Public$classForm")->$methodForm();
-}
-else
-{
-    $tabResponse["message"] = date("H:i:s") . "/$classForm/$methodForm";
+    echo json_encode($tabResponse);
 }
 
-echo json_encode($tabResponse);
