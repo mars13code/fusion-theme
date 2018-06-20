63adb1e1871101b23c5fd64ba7ab8013aebff756
test1 <test1@mail.me>
2018-06-20 18:01:45 +0200

    core init

---
+++ class/MyTheme.php
@@ -0,0 +1,25 @@
+<?php
+
+// file:        MyTheme.php
+// creation:    2018-06-20 17:57:41
+// licence:     MIT
+// author:      mars13.fr
+
+class MyTheme
+{
+    // TRAITS
+    use TraitOne;
+    
+    // PROPERTIES
+    
+    // METHODS
+    
+    // constructor
+    function __construct ()
+    {
+        echo "(MyTheme)";        
+    }
+    
+    //@end
+    
+}
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-echo "(theme)";
+// class autoload
+loadClass("300-theme", __DIR__);
+
+new MyTheme;
